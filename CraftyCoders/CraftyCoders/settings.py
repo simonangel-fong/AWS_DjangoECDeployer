110c21a379bd68e53ac4fbfbcfc1b155ecc8d32d
simonangel-fong <simonangelfong@gmail.com>
2023-10-25 12:22:54 -0400

    add resume and home page

--- CraftyCoders/CraftyCoders/settings.py
+++ CraftyCoders/CraftyCoders/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static'  # collectstatic
+STATICFILES_DIRS = [Path(BASE_DIR, 'CraftyCoders/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
