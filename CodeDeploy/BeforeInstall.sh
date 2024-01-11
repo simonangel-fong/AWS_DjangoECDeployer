39fc2272e0bdfcc9aa94f6011af0466704d83b39
simonangel-fong <simonangelfong@gmail.com>
2024-01-10 23:47:45 -0500

    alter role in appspec

--- CodeDeploy/BeforeInstall.sh
+++ CodeDeploy/BeforeInstall.sh
@@ -9,21 +9,21 @@
 #   script of before installation
 
 # Create the CICD log
-sudo rm -rf Deploy_BeforeInstall.log
-sudo touch /home/ubuntu/Deploy_BeforeInstall.log
+rm -rf Deploy_BeforeInstall.log
+touch /home/ubuntu/Deploy_BeforeInstall.log
 
 # log start cicd BeforeInstall
-sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') CodeDeploy BeforeInstall starting..." >>/home/ubuntu/Deploy_BeforeInstall.log
+echo -e "$(date +'%Y-%m-%d %H:%M:%S') CodeDeploy BeforeInstall starting..." >>/home/ubuntu/Deploy_BeforeInstall.log
 
 # remove the existing env, since each push might have different packages within env
-sudo rm -rf /home/ubuntu/env &&
-    sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') ENV - Remove existing env." >>/home/ubuntu/Deploy_BeforeInstall.log ||
-    sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') Fail: ENV - Remove existing env." >>/home/ubuntu/Deploy_BeforeInstall.log
+rm -rf /home/ubuntu/env &&
+    echo -e "$(date +'%Y-%m-%d %H:%M:%S') ENV - Remove existing env." >>/home/ubuntu/Deploy_BeforeInstall.log ||
+    echo -e "$(date +'%Y-%m-%d %H:%M:%S') Fail: ENV - Remove existing env." >>/home/ubuntu/Deploy_BeforeInstall.log
 
 # remove the existing repo
-sudo rm -rf /home/ubuntu/AWS_EC2_ECDjangoDeploy &&
-    sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') ENV - Remove existing env." >>/home/ubuntu/Deploy_BeforeInstall.log ||
-    sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') Fail: ENV - Remove existing env." >>/home/ubuntu/Deploy_BeforeInstall.log
+rm -rf /home/ubuntu/AWS_EC2_ECDjangoDeploy &&
+    echo -e "$(date +'%Y-%m-%d %H:%M:%S') ENV - Remove existing env." >>/home/ubuntu/Deploy_BeforeInstall.log ||
+    echo -e "$(date +'%Y-%m-%d %H:%M:%S') Fail: ENV - Remove existing env." >>/home/ubuntu/Deploy_BeforeInstall.log
 
 # log complete cicd BeforeInstall
-sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') CodeDeploy BeforeInstall completed." >>/home/ubuntu/Deploy_BeforeInstall.log
+echo -e "$(date +'%Y-%m-%d %H:%M:%S') CodeDeploy BeforeInstall completed." >>/home/ubuntu/Deploy_BeforeInstall.log
