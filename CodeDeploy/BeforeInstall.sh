7e2029ad1d2dc3411e000383a14e0dfad17616af
simonangel-fong <simonangelfong@gmail.com>
2024-01-11 22:52:23 -0500

    update BeforeInstall

--- CodeDeploy/BeforeInstall.sh
+++ CodeDeploy/BeforeInstall.sh
@@ -22,30 +22,30 @@ fi
 sudo touch "/home/ubuntu/log/deploy.log"
 
 # Log start CICD BeforeInstall
-echo -e "$(date +'%Y-%m-%d %H:%M:%S') CodeDeploy BeforeInstall starting..." >>"/home/ubuntu/log/deploy.log"
+sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') CodeDeploy BeforeInstall starting..." >>"/home/ubuntu/log/deploy.log"
 
 # Check if the env folder exists
 if [ -d "/home/ubuntu/env" ]; then
     # Remove the existing env folder and its contents
     rm -rf "/home/ubuntu/env" &&
-        echo -e "$(date +'%Y-%m-%d %H:%M:%S') Remove existing env." >>"/home/ubuntu/log/deploy.log" ||
-        echo -e "$(date +'%Y-%m-%d %H:%M:%S') Fail: Remove existing env." >>"/home/ubuntu/log/deploy.log"
+        sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') Remove existing env." >>"/home/ubuntu/log/deploy.log" ||
+        sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') Fail: Remove existing env." >>"/home/ubuntu/log/deploy.log"
 else
     # Log a message if the env folder doesn't exist
-    echo -e "$(date +'%Y-%m-%d %H:%M:%S') No existing env to remove." >>"/home/ubuntu/log/deploy.log"
+    sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') No existing env to remove." >>"/home/ubuntu/log/deploy.log"
 fi
 
 # Check if the AWS_ECDjangoDeployer folder exists
 if [ -d "/home/ubuntu/AWS_ECDjangoDeployer" ]; then
     # Remove the existing AWS_ECDjangoDeployer folder
     rm -rf "/home/ubuntu/AWS_ECDjangoDeployer" &&
-        echo -e "$(date +'%Y-%m-%d %H:%M:%S') Remove existing repo." >>"/home/ubuntu/log/deploy.log" ||
-        echo -e "$(date +'%Y-%m-%d %H:%M:%S') Fail: Remove existing repo." >>"/home/ubuntu/log/deploy.log"
+        sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') Remove existing repo." >>"/home/ubuntu/log/deploy.log" ||
+        sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') Fail: Remove existing repo." >>"/home/ubuntu/log/deploy.log"
 else
     # Log a message if the AWS_ECDjangoDeployer folder doesn't exist
-    echo -e "$(date +'%Y-%m-%d %H:%M:%S') No existing repo to remove." >>"/home/ubuntu/log/deploy.log"
+    sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') No existing repo to remove." >>"/home/ubuntu/log/deploy.log"
 fi
 
 # log complete cicd BeforeInstall
-echo -e "$(date +'%Y-%m-%d %H:%M:%S') CodeDeploy BeforeInstall completed.\n" >>"/home/ubuntu/log/deploy.log"
-echo -e " " >>"/home/ubuntu/log/deploy.log"
+sudo echo -e "$(date +'%Y-%m-%d %H:%M:%S') CodeDeploy BeforeInstall completed.\n" >>"/home/ubuntu/log/deploy.log"
+sudo echo -e " " >>"/home/ubuntu/log/deploy.log"
